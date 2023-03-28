82d2fbdca5f23251bd908b19321da6cc4da7e125
hongregii <98505239+hongregii@users.noreply.github.com>
2023-03-29 03:29:54 +0900

    feat-사이드바 로직 구현

--- modulers-react/src/layouts/Sidebar.tsx
+++ modulers-react/src/layouts/Sidebar.tsx
@@ -1,27 +1,125 @@
 import { StyledLink } from "../components/common/Buttons";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Sidebar = () => {
+  // ^ 걍 order/id 할 때 쓸 id값 랜덤으로 만든 것임..
   let someId = Math.random() * 100;
   someId = Math.floor(someId);
+
+  // state
+  const [selectedMenus, setSelectedMenus] = useState<ISelectedMenus>({
+    generate: false,
+    shop: false,
+    myPage: false,
+    order: false,
+  });
+  interface ISelectedMenus {
+    generate: boolean;
+    shop: boolean;
+    myPage: boolean;
+    order: boolean;
+  }
+
+  const handleLinkClick = (e) => {
+    const isOpen = selectedMenus[e.target.id];
+    setSelectedMenus({ ...selectedMenus, [e.target.id]: !isOpen });
+  };
+
   return (
     <BlackSidebar>
       <div>이것이 사이드바</div>
       <nav>
-        <StyledLink to={"/generate"}>
-          <MenuDiv>메뉴1</MenuDiv>
-        </StyledLink>
-        <StyledLink to={"/shop"}>
-          <MenuDiv>메뉴2</MenuDiv>
-        </StyledLink>
-        <StyledLink to={"/my-page"}>
-          <MenuDiv>메뉴3</MenuDiv>
-        </StyledLink>
-        <StyledLink to={`/order/${someId}`}>
-          <MenuDiv>메뉴4</MenuDiv>
-        </StyledLink>
+        <MenuDiv
+          onClick={handleLinkClick}
+          name="generate"
+          id="generate"
+          level={1}
+        >
+          메뉴1
+        </MenuDiv>
+        {selectedMenus.generate === true ? (
+          <ChildMenuList>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴1</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴2</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴3</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴4</div>
+            </StyledLink>
+          </ChildMenuList>
+        ) : (
+          <></>
+        )}
+        <MenuDiv onClick={handleLinkClick} name="shop" id="shop" level={1}>
+          메뉴2
+        </MenuDiv>
+        {selectedMenus.shop === true ? (
+          <ChildMenuList>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴1</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴2</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴3</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴4</div>
+            </StyledLink>
+          </ChildMenuList>
+        ) : (
+          <></>
+        )}
+        <MenuDiv onClick={handleLinkClick} name="myPage" id="myPage" level={1}>
+          메뉴3
+        </MenuDiv>
+        {selectedMenus.myPage === true ? (
+          <ChildMenuList>
+            <StyledLink to={`/my-page`}>
+              <div>자식메뉴1</div>
+            </StyledLink>
+            <StyledLink to={`/my-page`}>
+              <div>자식메뉴2</div>
+            </StyledLink>
+            <StyledLink to={`/my-page`}>
+              <div>자식메뉴3</div>
+            </StyledLink>
+            <StyledLink to={`/my-page`}>
+              <div>자식메뉴4</div>
+            </StyledLink>
+          </ChildMenuList>
+        ) : (
+          <></>
+        )}
+        <MenuDiv onClick={handleLinkClick} name="order" id="order" level={1}>
+          메뉴4
+        </MenuDiv>
+        {selectedMenus.order === true ? (
+          <ChildMenuList>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴1</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴2</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴3</div>
+            </StyledLink>
+            <StyledLink to={`/order/${someId}`}>
+              <div>자식메뉴4</div>
+            </StyledLink>
+          </ChildMenuList>
+        ) : (
+          <></>
+        )}
       </nav>
     </BlackSidebar>
   );
@@ -51,5 +149,14 @@ const MenuDiv = styled.div`
   border: 1px solid white;
   &:hover {
     background-color: white;
+  }
+`;
+
+const ChildMenuList = styled.div`
+  /* &:hover {
+    background-color: white;
+  } */
+  div {
+    color: white;
   }
 `;
