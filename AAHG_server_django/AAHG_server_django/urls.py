a425dde9315f47508894a5b5c22e54b6a95622f8
horz.d <he1236@ajou.ac.kr>
2023-04-06 15:46:14 +0900

    git 꼬임으로 인한 back이 client 레포에 들어온 것 삭제

--- AAHG_server_django/AAHG_server_django/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from AAHG_server_django.views.about import get_views_about
-
-urlpatterns = [
-    path('about/', get_views_about),
-]
--- AAHG_server_django/AAHG_server_django/utils/about.py
+++
@@ -1,2 +0,0 @@
-def get_utils_about():
-    return {'about': 'django가 about이란 Key로 내보낸 데이터 밸류'}
